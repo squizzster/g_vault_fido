4c73f96e64b443b91e6acd4c77cc88bb7a23bed5
root <squizzster@gmail.com>
2025-06-21 19:59:24 +0000

    ok

---
+++ lib/gv_hex.pm
@@ -0,0 +1,26 @@
+package gv_hex;
+use strict;
+use warnings;
+
+# encode: bytes to lowercase hex
+sub encode {
+    my ($bytes) = @_;
+    return unless defined $bytes && length $bytes;
+    # Unpack each byte to two-digit hex
+    my $hex = unpack('H*', $bytes);
+    return $hex;
+}
+
+# decode: hex string to bytes
+sub decode {
+    my ($hex) = @_;
+    return unless defined $hex && length $hex;
+    # Reject odd length or non-hex chars
+    return unless $hex =~ /\A[0-9a-fA-F]*\z/;
+    return if length($hex) % 2;
+    # Pack back to binary
+    my $bytes = pack('H*', $hex);
+    return $bytes;
+}
+
+1;
